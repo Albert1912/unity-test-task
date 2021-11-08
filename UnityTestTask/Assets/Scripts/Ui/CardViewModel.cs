9dcd212fe33e2d8eccd01fa48b73366aa4c5d4c1
Альберт Насртдинов <albert19971912@gmail.com>
2021-11-08 21:42:37 +0700

    Added Ui scripts

---
+++ UnityTestTask/Assets/Scripts/Ui/CardViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Interfaces;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ui
+{
+    public class CardViewModel : MonoBehaviour
+    {
+        [SerializeField] private RawImage _image;
+        public IDownloadedImageConsumer Consumer { get; private set; }
+
+        private void Awake()
+        {
+            if (_image == null)
+            {
+                throw new ArgumentNullException(nameof(_image));
+            }
+
+            Consumer = new DownloadedImageConsumer(this);
+        }
+
+        public void SetImage(Texture image)
+        {
+            if (image == null)
+            {
+                return;
+            }
+
+            _image.texture = image;
+        }
+
+        public void ResetToDefault()
+        {
+            _image.texture = null;
+        }
+    }
+}
---
+++ UnityTestTask/Assets/Scripts/Ui/CardViewModelCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Interfaces;
+using UnityEngine;
+
+namespace Ui
+{
+    public class CardViewModelCollection : MonoBehaviour
+    {
+        [SerializeField] private List<CardViewModel> _cardViewModels;
+
+        public void ResetCollectionToDefault()
+        {
+            foreach (var cardViewModel in _cardViewModels)
+            {
+                cardViewModel.ResetToDefault();
+            }
+        }
+
+    public List<IDownloadedImageConsumer> GetConsumers()
+        {
+            return _cardViewModels.Select(x => x.Consumer).ToList();
+        }
+    }
+}
