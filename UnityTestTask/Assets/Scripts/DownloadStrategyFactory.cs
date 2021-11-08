e9723fdbe766815ef7079fe7e70514d49f30375a
Альберт Насртдинов <albert19971912@gmail.com>
2021-11-08 21:39:43 +0700

    Implemented factory

---
+++ UnityTestTask/Assets/Scripts/DownloadStrategyFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Enums;
+using Interfaces;
+
+public class DownloadStrategyFactory : IDownloadStrategyFactory
+{
+    private readonly Dictionary<DownloadStrategyType, IDownloadStrategy> _downloadStrategies;
+
+    public DownloadStrategyFactory(Dictionary<DownloadStrategyType, IDownloadStrategy> downloadStrategies)
+    {
+        _downloadStrategies = downloadStrategies;
+    }
+
+    public IDownloadStrategy Provide(DownloadStrategyType strategyType)
+    {
+        if (_downloadStrategies.TryGetValue(strategyType, out var strategy))
+        {
+            return strategy;
+        }
+
+        throw new ArgumentException($"Strategy for type {strategyType} not found");
+    }
+}
